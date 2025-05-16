5051d600c9bb0638e815305465df59a02ec76c49
only1Ksy <winterksy@gmail.com>
2025-05-16 21:30:59 +0900

    refactor: apply debouncing to search input handler

--- src/app/(routes)/search/_components/search-input.tsx
+++ src/app/(routes)/search/_components/search-input.tsx
@@ -3,27 +3,28 @@ import DeleteIcon from '@public/icons/search/delete.svg';
 
 interface Props {
   keyword: string;
-  handleSearch: (kwText: string) => void;
-  setKeyword: (kw: string) => void;
+  handleChange: (text: string) => void;
   handleKeyDown: (e: React.KeyboardEvent<HTMLInputElement>) => void;
+  handleSearch: () => void;
+  handleClear: () => void;
 }
 
-export default function SearchInput({ keyword, handleSearch, setKeyword, handleKeyDown }: Props) {
-  const hasInput = keyword === '' ? false : true;
+export default function SearchInput({ keyword, handleChange, handleKeyDown, handleSearch, handleClear }: Props) {
+  const hasInput = keyword.trim() !== '';
 
   return (
     <div className="bg-background-02 mt-11 flex h-13 w-full items-center">
-      <div className="ml-4" onClick={() => handleSearch(keyword)}>
+      <div className="ml-4" onClick={handleSearch}>
         <SearchIcon className="text-background-03" />
       </div>
       <input
         value={keyword}
-        onChange={(e) => setKeyword(e.target.value)}
+        onChange={(e) => handleChange(e.target.value)}
         onKeyDown={handleKeyDown}
         placeholder="search for a show, movie, genre, e.t.c."
         className="m-4 w-full placeholder-[color:var(--color-background-03)] focus:outline-none"
       />
-      <div className="mr-4" onClick={() => handleSearch('')}>
+      <div className="mr-4" onClick={handleClear}>
         <DeleteIcon className={`text-background-03 ${hasInput ? 'cursor-pointer' : ''}`} />
       </div>
     </div>
